374b2945e5f467bacf7e75f40a7169946e2c1312
Amos <2568986399@qq.com>
2026-02-09 01:19:20 +0800

    反馈页面还原成不用输入提交

--- app/src/main/java/com/snapreceipt/io/ui/me/feedback/FeedbackActivity.kt
+++ app/src/main/java/com/snapreceipt/io/ui/me/feedback/FeedbackActivity.kt
@@ -1,18 +1,10 @@
 package com.snapreceipt.io.ui.me.feedback
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import com.snapreceipt.io.R
 import com.snapreceipt.io.databinding.ActivityFeedbackBinding
 import com.snapreceipt.io.ui.common.EdgeToEdgeActivity
 
 class FeedbackActivity : EdgeToEdgeActivity() {
-
-    companion object {
-        private const val MAX_INPUT_LENGTH = 1000
-    }
-
     private var _binding: ActivityFeedbackBinding? = null
     private val binding get() = _binding!!
 
@@ -20,19 +12,7 @@ class FeedbackActivity : EdgeToEdgeActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityFeedbackBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         binding.pageHeader.setOnLeftIconClickListener { finish() }
-
-        binding.feedbackCharCount.text = getString(R.string.char_count_format, 0, MAX_INPUT_LENGTH)
-
-        binding.feedbackInput.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-            override fun afterTextChanged(s: Editable?) {
-                val length = s?.length ?: 0
-                binding.feedbackCharCount.text = getString(R.string.char_count_format, length, MAX_INPUT_LENGTH)
-            }
-        })
     }
 
     override fun onDestroy() {
